0799f9d6bf081cbcb5e2c7bf740f0b778260a671
Ashwin-28 <mailtoashwin28@gmsil.com>
2026-03-04 20:18:24 +0530

    out

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,9 +1,8 @@
 public class AddNumbers {
     public static void main(String[] args) {
         int a = 2;
-        int b = 4;
+        int b = 40;
         int sum = a + b;
         System.out.println("Sum = " + sum);
-        // Final test
     }
 }
