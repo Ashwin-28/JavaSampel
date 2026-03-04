339a269c221ef12a4c80841f84ba6300d578e82e
Ashwin-28 <mailtoashwin28@gmsil.com>
2026-03-04 19:38:50 +0530

    numbers have been changed

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,7 +1,7 @@
 public class AddNumbers {
     public static void main(String[] args) {
-        int a = 20;
-        int b = 40;
+        int a = 2;
+        int b = 4;
         int sum = a + b;
         System.out.println("Sum = " + sum);
     }
