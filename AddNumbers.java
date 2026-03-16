d68ce30f7c1c70692ec689d3c3e10bbeb08d5184
Ashwin S <166545132+Ashwin-28@users.noreply.github.com>
2026-03-16 09:47:28 +0530

    Remove sum calculation and print statement

--- AddNumbers.java
+++ AddNumbers.java
@@ -1,8 +1,5 @@
 public class AddNumbers {
     public static void main(String[] args) {
-        int a = 20;
-        int b = 40;
-        int sum = a + b;
-        System.out.println("Sum = " + sum);
+        System.out.println("Hello World");
     }
 }
